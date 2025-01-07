168dd3c6561b80788867b1650095bdb947542a0f
radiophysiker <bolshakovdmitry@bk.ru>
2025-01-07 13:22:25 +0300

    rewrite response

--- 2-random-api/handlers.go
+++ 2-random-api/handlers.go
@@ -9,6 +9,7 @@ import (
 
 func RandomHandler(w http.ResponseWriter, r *http.Request) {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	fmt.Fprint(w, random.Intn(6)+1)
+	randomNum := random.Intn(6) + 1
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(fmt.Sprintf("%d", randomNum)))
 }
